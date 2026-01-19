8fa8645c76ea115bd97133bce9b0a62ad802f416
KereszturiHanna <kerhanna05@gmail.com>
2026-01-19 12:02:51 +0100

    Konyhai eszköz hero img méretezése

--- konyhaiEszkozok.php
+++ konyhaiEszkozok.php
@@ -1,20 +1,23 @@
 <?php include "head.php"; ?>
 
 <section class="mx-auto max-w-[2000px] min-w-[280px] py-8 px-4 sm:px-8 md:px-10 lg:0px-20 ">
-    
+
     <div class="w-full rounded-[50px] sm:rounded-[60px] md:rounded-[70px] lg:rounded-[80px] bg-[#EBF4DD] flex flex-col lg:flex-row  items-center  py-6 sm:py-9 md:py-11 lg:py-14  px-2  sm:px-4 md:px-8 xl:px-16">
 
-        <img class="w-full rounded-[50px] ml-6 lg:w-1/2 xl:w-full max-w-[550px] lg:order-2" src="images/KonyhaiEszközök.jpg" alt="Konyhai eszközök kép" title="Konyhai eszközök kép">
+        <img class="w-full max-w-[280px] sm:max-w-[320px] md:max-w-[360px] lg:max-w-[400px] xl:max-w-[410px] rounded-[50px] ml-0 lg:ml-6 lg:order-2" src="images/KonyhaiEszközök.jpg" alt="Konyhai eszközök kép" title="Konyhai eszközök kép">
+
         <div class="text-center md:text-left">
 
-            <h1
-                class="text-4xl leading-[48px] md:text-5xl md:leading-[58px] lg:text-[50px] lg:leading-[70px] font-bold mb-6 md:mb-12">
+            <h1 class="text-4xl leading-[48px] md:text-5xl md:leading-[58px] lg:text-[50px] lg:leading-[70px] font-bold mb-6 md:mb-12">
                 Elevate Your Style with Modern Design.
             </h1>
+
             <span class="text-xl leading-[34px] underline font-semibold sm:text-[24px] mb-3 mt-5">
                 Discover Unique Creations!
             </span>
+
             <br>
+
             <p class="text-xl leading-[27px]  font-normal sm:text-[24px] mb-8 md:mb-12">
                 Explore innovative designs crafted with precision and elegance to enhance your lifestyle.
             </p>
