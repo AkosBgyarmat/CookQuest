e65d79a13109d6e0a861ef1271639b15346cdcf5
Keresztúri Hanna <100832885+hancsika15@users.noreply.github.com>
2026-01-22 16:17:05 +0100

    Footer

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
         <!-- Logo & Description -->
         <div class="border-[black]">
             <h2 class="text-2xl  font-bold mb-4 text-white">CookQuest</h2>
-            <p class="mb-4">Creating innovative solutions for a better future. Stay connected with us!</p>
+            <p class="mb-4 max-w-[250px]">Tanulj meg főzni lépésről lépésre és ismerkedj meg a konyhai eszközökkel!</p>
         </div>
         
         <!-- Links -->
@@ -39,17 +39,19 @@
         
         <!-- Contact Info -->
         <div>
-            <h3 class="text-xl font-semibold mb-4">Contact Us</h3>
-            <p class="mb-2">123 Main Street, City, Country</p>
-            <p class="mb-2">Email: info@yourbrand.com</p>
+            <h3 class="text-xl font-semibold mb-4">Lépj kapcsolatba velünk!</h3>
+            <p class="mb-2">2660 Balassagyarmat, Rákóczi fejedelem út 50</p>
+            <p class="mb-2">Email: info@cookquest.com</p>
             <p>Phone: +123 456 7890</p>
         </div>
+
     </div>
 
     <!-- Copyright -->
     <div class="mt-12 text-center text-gray-400 text-sm relative z-10">
         &copy; 2026 CookQuest. Minden jog fenntartva.
     </div>
+
 </footer>
 
 
