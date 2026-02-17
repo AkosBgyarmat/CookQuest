41bb6005c11c9d1d5e379995999e84f56a0c2bcf
Keresztúri Hanna <100832885+hancsika15@users.noreply.github.com>
2026-02-18 00:05:47 +0100

    Országok betöltése a regisztrációhoz

---
+++ views/autentikacio/orszag.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-Type:application/json; charset-utf-8'); //a válasz típusa JSON lesz és a karakter kódolás utf-8
+
+/** 
+ * require: nem engedi hiba esetén betölteni az oldalt
+ * include: engedi betölteni az oldalt
+ */
+require "../kapcsolat.php"; //kapcsolódás az adatbázishoz
+
+//SQL lekérdezés az adatbázis táblából a rekordok betöltése
+$sql = "
+    SELECT 
+    OrszagID AS Id, 
+    Elnevezes FROM orszag;
+";
+
+//Végrehajtjuk a lekérdezést
+$result = mysqli_query($conn, $sql);
+//var_dump($result);
+
+//Létrehozunk egy tömböt az adatok tárolására
+$data = [];
+
+$row = mysqli_num_rows($result) > 0;
+//var_dump($row);
+
+//ha van eredmény és legalább 1 sor van akkor olvasuk be soronként
+if ($result && $row > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+        //var_dump($data);
+    }
+}
+
+echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
