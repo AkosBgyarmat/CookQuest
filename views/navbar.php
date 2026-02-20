ef3096929df1cbe2b424e060c0ac88d7788a44fd
KereszturiHanna <kerhanna05@gmail.com>
2026-02-20 08:27:40 +0100

    Keresőmező

--- views/navbar.php
+++ views/navbar.php
@@ -30,7 +30,7 @@
             type="text" name="query" id="query" />
         <button type="submit"
             class="absolute inline-flex items-center h-8 px-2 py-2 text-sm text-white transition duration-150 ease-in-out rounded-full outline-none right-3 top-1 bg-[#5A7863] sm:px-6 sm:text-base sm:font-medium hover:bg-[#5A7863] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#5A7863]">
-            <svg class="-ml-0.5 sm:-ml-1 mr-2 w-4 h-4 sm:h-5 sm:w-5" xmlns="http://www.w3.org/2000/svg" fill="none"
+            <svg class=" sm:-ml-1 w-4 h-4 sm:h-5 sm:w-5" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
