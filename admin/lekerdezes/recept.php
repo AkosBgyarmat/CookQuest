46414b4ea02cbb5fb48f5c8fafa1d10ff30b2765
KereszturiHanna <kerhanna05@gmail.com>
2026-03-16 10:42:52 +0100

    Recept megjelenítése az admin oldalon

---
+++ admin/lekerdezes/recept.php
@@ -0,0 +1,39 @@
+<?php
+
+require "../../kapcsolat.php"; //kapcsolódás az adatbázishoz
+
+//SQL lekérdezés az adatbázis táblából a rekordok betöltése
+$sql = "
+SELECT 
+    r.ReceptID AS id,
+    r.Nev,
+    r.Kep,
+    r.ElkeszitesiIdo,
+    ns.Szint AS Nehezseg,
+    r.BegyujthetoPontok,
+    r.Adag,
+    r.Elkeszitesi_leiras,
+    em.ElkeszitesiMod AS ElkeszitesiMod,
+    ark.Arkategoria AS Arkategoria,
+    ak.Alkategoria AS Alkategoria,
+    r.Kaloria,
+    r.BegyujthetoPontok
+FROM recept r
+JOIN nehezsegiszint ns ON r.NehezsegiSzintID = ns.NehezsegiSzintID
+JOIN alkategoria ak ON r.AlkategoriaID = ak.AlkategoriaID
+JOIN elkeszitesimod em ON r.ElkeszitesiModID = em.ElkeszitesiModID
+JOIN arkategoria ark ON r.ArkategoriaID = ark.ArkategoriaID
+ORDER BY r.ReceptID ASC;
+";
+
+$result = mysqli_query($conn, $sql);
+
+$data = [];
+
+if ($result && mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+}
+
+echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
