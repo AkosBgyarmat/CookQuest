d591a958e8e481dc0496ac20060983147a9465af
Keresztúri Hanna <100832885+hancsika15@users.noreply.github.com>
2026-03-16 23:05:44 +0100

    Hamburger menu hozzáadása

---
+++ admin/js/hamburger.js
@@ -0,0 +1,21 @@
+function toggleSidebar() {
+
+    const sidebar = document.getElementById("sidebar");
+    const button = document.getElementById("hamburgerBtn");
+
+    sidebar.classList.toggle("-translate-x-full");
+
+    if (sidebar.classList.contains("-translate-x-full")) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+
+}
+
+function goToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
